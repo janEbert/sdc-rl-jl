6d943719679ff50b4d8acd7770f9432329f3dec5
janEbert <janpublicebert@posteo.net>
2021-02-24 15:22:06 +0100

    Add another variant of complex relu

--- CFlux.jl
+++ CFlux.jl
@@ -2,6 +2,8 @@ import Random
 
 import Flux
 
+# Layers
+
 function LSTMCell(in::Integer, out::Integer; initW, initb, init_state)
     cell = Flux.LSTMCell(initW(out * 4, in), initW(out * 4, out), initb(out * 4),
                          init_state(out), init_state(out))
@@ -10,6 +12,8 @@ function LSTMCell(in::Integer, out::Integer; initW, initb, init_state)
 end
 
 LSTM(a...; ka...) = Flux.Recur(LSTMCell(a...; ka...))
+
+# Initialization
 
 function glorot_uniform(rng::Random.AbstractRNG, type::Type{<:Number}, dims...)
     (rand(rng, type, dims...) .- 0.5) .* sqrt(24.0 / sum(Flux.nfan(dims...)))
@@ -31,8 +35,14 @@ function init_bias(type::Type{<:Number})
     (dims...) -> init_bias(type, dims...)
 end
 
+# Activation
+
 function crelu(x)
     ifelse(real(x) >= 0 && imag(x) >= 0, x, zero(x))
+end
+
+function crelu2(x)
+    Flux.relu(real(x)) + Flux.relu(imag(x)) * im
 end
 
 function sigmoid(x)
